d1d89d5e17593089070464af5938852bd146106e
raul.espim <devraespim@gmail.com>
2026-04-05 00:26:14 -0300

    remove unused codes

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -25,6 +27,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+    buildFeatures {
+        compose = true
     }
 }
 
